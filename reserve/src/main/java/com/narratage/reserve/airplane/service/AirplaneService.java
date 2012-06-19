ceb2c5d56729b6a40e73de28d66cd2237c107be4
changhwa <changhwaoh.co@gmail.com>
2012-06-19 15:25:26 +0900

    merge시 충돌난 소스 수정

--- reserve/src/main/java/com/narratage/reserve/airplane/service/AirplaneService.java
+++ reserve/src/main/java/com/narratage/reserve/airplane/service/AirplaneService.java
@@ -7,13 +7,8 @@ public interface AirplaneService {
 	public boolean insertAirplane(HashMap map);
 	public boolean modifyAirplane(int airplaneCode);
 	public ArrayList findAllAirplane();
-<<<<<<< HEAD
 	public ArrayList findAirplane(int airplaneCode);
 	public boolean deleteAirplane(int airplaneCode);
 	public String findSeat(int airplaneCode);
 	public boolean modifySeat(int airplaneCode);
-=======
-	public ArrayList findAirplane(String airplaneCode);
-	public boolean deleteAirplane(String airplaneCode);
->>>>>>> master
 }
