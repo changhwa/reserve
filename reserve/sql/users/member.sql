c0ba486f48b1865332d837ccd1c20224d7e846c7
imissyoubrad <imissyoubrad@gmail.com>
2012-06-23 17:57:34 +0900

    create member table

---
+++ reserve/sql/users/member.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phone` varchar(30) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `passport_number` varchar(30) DEFAULT NULL,
+  `passport_date` date DEFAULT NULL,
+  `passport_country` tinyint(4) DEFAULT NULL,
+  `jumin` varchar(30) DEFAULT NULL,
+  `sex` bit(1) DEFAULT NULL,
+  `job` varchar(30) DEFAULT NULL,
+  `answer` varchar(100) DEFAULT NULL,
+  `mileage` double DEFAULT NULL,
+  `receive_mail` bit(1) DEFAULT NULL,
+  `receive_sms` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
