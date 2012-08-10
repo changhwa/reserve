e7cb2bd7882990660e48c5f797273fe187359c74
StevePak <StevePak@StevePak-PC>
2012-08-10 23:03:14 +0900

    airlineScheduleService 길찾기는 완료 가장 최적화된 경로 가져오기는 미완성

--- reserve/src/main/java/com/narratage/reserve/inform/service/AirlineScheduleServiceImpl5.java
+++ reserve/src/main/java/com/narratage/reserve/inform/service/AirlineScheduleServiceImpl5.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,12 +18,15 @@ import com.narratage.reserve.inform.datatype.AirportType;
 import com.narratage.reserve.inform.datatype.DateType;
 import com.narratage.reserve.inform.domain.AirlineSchedule;
 import com.narratage.reserve.inform.util.Table;
+
 @Service("AirlineScheduleService")
 public class AirlineScheduleServiceImpl5 implements AirlineScheduleService {
 	@Autowired
 	private AirlineScheduleDao airlineScheduleDao;
 
-	public static final int LIMIT_TRANSFER_AIRPORT_NUMBER = 3;
+	// 2번이상 비행기를 갈아타지 못하도록 한다.
+	public static final int LIMIT_TRANSFER_AIRPORT_NUMBER = 2;
+
 	// 36시간 이상 걸리는 경우는 무시한다
 	public static final long TRAVEL_LIMIT_DATE = 36 * 60 * 60 * 1000;
 	public static final long ONE_DAY = 24 * 60 * 60 * 1000;
@@ -35,12 +39,12 @@ public class AirlineScheduleServiceImpl5 implements AirlineScheduleService {
 	private static final String WORKED = "WORKED";
 
 	private static final int ROOT_PARENT_INDEX = Integer.MIN_VALUE;
-	
+
 	public List<AirlineSchedule> getSortedAirlineScheduleList(String fromAirportIATA, String toAirportIATA,
 			Date startArriveDate, Date endArriveDate) {
 		// Table class를 활용한다. Table에는 총 4개의 컬럼이 필요하다.
 		// parentIndex, IATA, BeginLandingDate, EndLandingDate
-		// ,해당 String값은 헷갈리기 쉬우므로 상수값으로 지정하여 사용한다.
+		// 해당 String값은 헷갈리기 쉬우므로 상수값으로 지정하여 사용한다.
 		Table<Integer, String, Object> routeTable = new Table<Integer, String, Object>();
 
 		int scheduleTableIndex = 0;
@@ -67,7 +71,12 @@ public class AirlineScheduleServiceImpl5 implements AirlineScheduleService {
 			}
 		};
 
-		for (int routeIndex = 0; routeIndex < 15; routeIndex++) {
+		int routeIndex = 0;
+		boolean hasNext = true;
+
+		while (hasNext = true) {
+
+			// 루프문에서 임시로 사용할 변수의 값을 저장한다.
 			tmpIndex = scheduleTableIndex;
 			tmpParentIndex = (Integer) routeTable.get(routeIndex, PARENT_INDEX);
 			tmpIATA = (String) routeTable.get(routeIndex, IATA);
@@ -76,10 +85,12 @@ public class AirlineScheduleServiceImpl5 implements AirlineScheduleService {
 
 			List<AirlineSchedule> tmpSchedule = airlineScheduleDao.get(AirportType.LANDING, DateType.LANDING, tmpIATA,
 					tmpBeginDate, tmpEndDate);
+
 			System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-			for(AirlineSchedule s : tmpSchedule){
+			for (AirlineSchedule s : tmpSchedule) {
 				System.out.println(s);
 			}
+
 			// 결과값을 분류한다.
 			tmpScheduleMap.clear();
 			tmpScheduleMap = new HashMap<String, List<AirlineSchedule>>();
@@ -108,41 +119,79 @@ public class AirlineScheduleServiceImpl5 implements AirlineScheduleService {
 			for (String Iata : parentsIndexList) {
 				System.out.println("부모 공항 : " + Iata);
 			}
-			
+
 			System.out.println("=======================");
 
 			String iteratedIata;
-			while (iter.hasNext()) {
-				iteratedIata = iter.next();
-				// 부모값을 체크하여 이미 한번 거쳤던 공항과 현재 공항이 출발공항일 경우 더이상 길찾기를 진행하지 않도록 한다.
-				System.out.println("iteratedIata.equals(fromAirportIATA) : "+ iteratedIata.equals(fromAirportIATA));
-				System.out.println("parentsIndexList.contains(iteratedIata))  : "+ parentsIndexList.contains(iteratedIata));
-				System.out.println(((iteratedIata.equals(fromAirportIATA) || parentsIndexList.contains(iteratedIata)) == false));
-				if (parentsIndexList.contains(iteratedIata) == false) {
-					List<AirlineSchedule> tmpInnerScheduleList = tmpScheduleMap.get(iteratedIata);
-					
-					System.out.println("-------------------------------------------");
-					for(AirlineSchedule schedule : tmpInnerScheduleList){
-						System.out.println(schedule);
-					}
-					
-					Collections.sort(tmpInnerScheduleList, scheduleComparator);
 
-					scheduleTableIndex += 1;
-					// 정렬 뒤 각각의 값에서 나온값을 바탕으로 계층구조 Table에 값을 넣어준다.
-					routeTable.put(scheduleTableIndex, PARENT_INDEX, tmpIndex);
-					routeTable.put(scheduleTableIndex, IATA, iteratedIata);
-					routeTable.put(scheduleTableIndex, BEGIN_LANDING_DATE, new Date(startArriveDate.getTime()
-							- TRAVEL_LIMIT_DATE));
-					routeTable.put(scheduleTableIndex, END_LANDING_DATE,
-							tmpInnerScheduleList.get(tmpInnerScheduleList.size() - 1).getLandingDate());
-						
-				}
+			// 이미 공항을 한번 거쳤는지 체크한다.
+			boolean isNotInParentsList = false;
+			// 제한횟수보다 환승을 많이 하였는지를 체크한다.
+			boolean isNotLimitTransfer = false;
+
+			while (iter.hasNext()) {
+				// 다음공항의 IATA값을 가지고 온다.
+				iteratedIata = iter.next();
+
+				// 부모값을 체크하여 이미 한번 거쳤던 공항과 현재 공항이 출발공항일 경우 더이상 길찾기를 진행하지 않도록 한다.
+				if (parentsIndexList.contains(iteratedIata) == false)
+					isNotInParentsList = true;
+				// 환승제한보다 적게 움직였거나 환승제한일때 출발공항인 경우만 인정.
+				if (parentsIndexList.size() < LIMIT_TRANSFER_AIRPORT_NUMBER
+						|| (parentsIndexList.size() < LIMIT_TRANSFER_AIRPORT_NUMBER && iteratedIata
+								.equals(fromAirportIATA)))
+
+					if (parentsIndexList.contains(iteratedIata) == false) {
+						List<AirlineSchedule> tmpInnerScheduleList = tmpScheduleMap.get(iteratedIata);
+						System.out.println("-------------------------------------------");
+
+						for (AirlineSchedule schedule : tmpInnerScheduleList) {
+							System.out.println(schedule);
+						}
+
+						Collections.sort(tmpInnerScheduleList, scheduleComparator);
+
+						scheduleTableIndex += 1;
+						// 정렬 뒤 각각의 값에서 나온값을 바탕으로 계층구조 Table에 값을 넣어준다.
+						routeTable.put(scheduleTableIndex, PARENT_INDEX, tmpIndex);
+						routeTable.put(scheduleTableIndex, IATA, iteratedIata);
+						routeTable.put(scheduleTableIndex, BEGIN_LANDING_DATE, new Date(startArriveDate.getTime()
+								- TRAVEL_LIMIT_DATE));
+						routeTable.put(scheduleTableIndex, END_LANDING_DATE,
+								tmpInnerScheduleList.get(tmpInnerScheduleList.size() - 1).getLandingDate());
+						routeTable.put(scheduleTableIndex, WORKED, false);
+
+						if (iteratedIata.equals(toAirportIATA)) {
+							routeTable.put(scheduleTableIndex, WORKED, true);
+						}
+					}
 			}
 			// 작업이 완료된 뒤 WORKED컬럼값을 TRUE로 변경하여준다.
-			routeTable.put(scheduleTableIndex, WORKED, true);
+			routeTable.put(routeIndex, WORKED, true);
 		}
-		
+
+		// routeTable의 WORKED컬럼에 아직 작업하지 않은 값이 있는지 확인한다.
+		if (routeTable.getByColumn(WORKED).containsValue(false) == true) {
+			hasNext = true;
+			// 다음에 작업할 인덱스 값을 구해준다.
+			Iterator<Integer> workedKeyIter = routeTable.getByColumn(WORKED).keySet().iterator();
+			int workedKey;
+			Date tmpFastestDate = new Date(Long.MAX_VALUE);
+			Date tmpForIterDate;
+			while (workedKeyIter.hasNext()) {
+				workedKey = workedKeyIter.next();
+				if ((Boolean) routeTable.get(workedKey, WORKED) == false) {
+					tmpForIterDate = ((Date) routeTable.get(workedKey, END_LANDING_DATE));
+					if (tmpForIterDate.getTime() < tmpFastestDate.getTime()) {
+						routeIndex = workedKey;
+						tmpFastestDate = tmpForIterDate;
+					}
+				}
+			}
+		} else {
+			hasNext = false;
+		}
+
 		// 최종검증
 		System.out.println("========================================");
 		for (int idx = 0; idx < routeTable.rowSize(); idx++) {
@@ -150,6 +199,18 @@ public class AirlineScheduleServiceImpl5 implements AirlineScheduleService {
 					+ (Date) routeTable.get(idx, BEGIN_LANDING_DATE) + " 도착시간="
 					+ (Date) routeTable.get(idx, END_LANDING_DATE));
 		}
+		
+		
+		/********************************************************************/
+		/************************길찾기 완료*******************************/
+		/********************************************************************/
+
+		//routeTable에서 IATA컬럼이 출발 공항인 경우를 찾습니다.
+		//routeTable.getByColumn(fromAirportIATA)
+		
+		//부모값을 하나씩 찾아가며 해당하는 스케쥴을 들고 오며, 해당하는 값들의 
+		
+		
 
 		return null;
 	}
