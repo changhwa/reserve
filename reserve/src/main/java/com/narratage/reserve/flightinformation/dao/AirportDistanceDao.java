1ee3314083fa3143ec468aae173d2f757ee8d48b
StevePak <StevePak@StevePak-PC>
2012-09-18 22:38:20 +0900

    Delete unuse package (*.flightinformation.*)

--- reserve/src/main/java/com/narratage/reserve/flightinformation/dao/AirportDistanceDao.java
+++
@@ -1,6 +0,0 @@
-package com.narratage.reserve.flightinformation.dao;
-
-public interface AirportDistanceDao{
-	public double getDistance(String IATA01, String IATA02);
-	public void addDistance(String IATA01, String IATA02, double distance);	
-}
